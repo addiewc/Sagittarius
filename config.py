445f60649c2fd72e300ad388a0e82420b26682a5
Addie Chambers <addiewc@cs.washington.edu>
2022-08-02 17:20:44 -0700

    Initial configuration file for model experiments in Sagittarius

---
+++ config.py
@@ -0,0 +1,7 @@
+"""
+Configuration file for Sagittarius.
+"""
+
+# Evo-Devo related file locations
+EVO_DEVO_DATA_LOC = ''  # put EvoDevo dataset location here
+HAGR_AGING_GENES_LOC = ''  # put HAGR aging genes `dataset_indices.txt` file here
